394c21af7403f9eefc091c2a2c339ab875353fe8
Daniel Ludolf <daniel@forsythfutures.org>
2025-03-28 15:49:06 -0400

    initial commit

---
+++ import_data.R
@@ -0,0 +1,30 @@
+
+library(tidyverse)
+
+# read in entity registration data for those registered in SAM to do business with the federal govt
+  # https://sam.gov/data-services/Entity%20Registration/Public%20V2?privacy=Public
+  # NOTE I had to manually download data
+dat <- read.table("~/contractors-list/SAM_PUBLIC_MONTHLY_V2_20250302.dat", header = F, sep = "|",
+                  fill = T, quote = "", stringsAsFactors = F, skip = 1)
+
+dat %>% 
+  # select company name, address information, and the company's website
+  select(V12, V16:V21, V27) %>% 
+  # trim whitespace
+  mutate(across(everything(), ~ .x %>% str_squish())) %>%
+  # condense columns related to the company's address into one column
+  mutate(`Company Address` = if_else(
+    is.na(V21) | V21 == "", 
+    str_glue("{V16} {V17} {V18}, {V19} {V20}"),
+    str_glue("{V16} {V17} {V18}, {V19} {V20}-{V21}")),
+    # place website names in all lowercase
+    V27 = str_to_lower(V27)) %>% 
+  # renaming columns and adjusting how they're presented
+  rename(`Company Name` = V12, `Company Website` = V27) %>% 
+  select(-V16:-V21, `Company Name`, `Company Website`, `Company Address`) %>% 
+  # trim whitespace again
+  mutate(across(everything(), ~ .x %>% str_squish())) %>%
+  # remove duplicate rows 
+  distinct() %>%
+  # write csv file
+  write_csv("~/contractors-list/contractors_list.csv")
